591f2d85b8babe30bffd091a4cb9dc872dcd5a84
Harshal Patel <46495579+harshalstomp@users.noreply.github.com>
2020-08-19 22:21:54 +0530

    Update pwdb.py

--- pwdb.py
+++ pwdb.py
@@ -5,35 +5,45 @@ from pandastable import Table, TableModel
 import pandas as pd
 import os
 
+#Calling the delete.py script
+def delete():
+   os.system('python del.py')
 
-def generate():
-    print("HI")
-    
+#Calling the view.py script    
 def view():
    os.system('python view.py')
-   
+
+#Calling the add.py script   
 def add():
-    #root1.destroy()
     os.system('python add.py')
     
-    
-    
+#Creating and positioning the window
 root1 = Tk(className='Database Window')
 root1.geometry('300x240')
+root1.resizable(False,False)
+windowWidth = root1.winfo_reqwidth()
+windowHeight = root1.winfo_reqheight()
+positionRight = int(root1.winfo_screenwidth()/2 - windowWidth/2)-120
+positionDown = int(root1.winfo_screenheight()/2 - windowHeight/2)-75
+root1.geometry("+{}+{}".format(positionRight, positionDown))
 
+#View Database button
 Font1 = font.Font(family='Helvetica Neue', size=10, weight='bold'  )
 viewButton = Button(root1, text='View Credentials',command=view,padx=30,pady=10,bg='#2b2b2b',fg='#ffffff',activebackground='#686868',activeforeground='#ffffff')
 viewButton['font'] = Font1 
 viewButton.grid(row=0,column=0,padx=40,pady=15)
 
+#Add credentials button
 addButton = Button(root1, text='Add Credentials',command=add,padx=31,pady=10,bg='#2b2b2b',fg='#ffffff',activebackground='#686868',activeforeground='#ffffff')
 addButton['font'] = Font1 
 addButton.grid(row=1,column=0,padx=40,pady=15)
 
-deleteButton = Button(root1, text='Delete Credentials',command=generate,padx=28,pady=10,bg='#2b2b2b',fg='#ffffff',activebackground='#686868',activeforeground='#ffffff')
+#Deleting the credentials
+deleteButton = Button(root1, text='Delete Credentials',command=delete,padx=28,pady=10,bg='#2b2b2b',fg='#ffffff',activebackground='#686868',activeforeground='#ffffff')
 deleteButton['font'] = Font1 
 deleteButton.grid(row=2,column=0,padx=40,pady=15)
 
+#Exit button
 exit_btn = PhotoImage(file='images/exit.png')
 exit_image = Label(image = exit_btn)
 exit_button = Button(root1, image = exit_btn , command=root1.destroy, borderwidth = 0)
