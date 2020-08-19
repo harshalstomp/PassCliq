e5e4e34b06ca6218bc1015199790e8e8d678b1d0
Harshal Patel <46495579+harshalstomp@users.noreply.github.com>
2020-08-19 22:21:00 +0530

    Update del.py

--- del.py
+++ del.py
@@ -4,6 +4,7 @@ import tkinter.font as font
 import pandas as pd
 import csv
 
+#Deleting the credentials
 def getvals1():
     try:
         vault = pd.read_csv('vault.csv')
@@ -35,16 +36,25 @@ def getvals1():
         vault.to_csv('vault.csv', index=True)
     
 
+#Creating and positioning the window
 root2 = Tk()
 root2.geometry('350x200')
+root2.resizable(False,False)
+windowWidth = root2.winfo_reqwidth()
+windowHeight = root2.winfo_reqheight()
+positionRight = int(root2.winfo_screenwidth()/2 - windowWidth/2)-120
+positionDown = int(root2.winfo_screenheight()/2 - windowHeight/2)-75
+root2.geometry("+{}+{}".format(positionRight, positionDown))
 
 Font1 = font.Font(family='Helvetica Neue', size=10, weight='bold'  )
 
 zz = Label(root2,text='')
 zz.grid(row=1)
 
+#Service_name Entry
 service_name = Label(root2, text="SERVICE NAME : ")
 service_name['font'] = Font1 
+#Username Entry
 username = Label(root2, text="      USERNAME : ")
 username['font'] = Font1 
 
@@ -60,6 +70,7 @@ username_entry = Entry(root2, textvariable=username_value,width=30)
 service_entry.grid(row=2, column=3)
 username_entry.grid(row=4, column=3)
 
+#SUbmit Button
 submit=Button(root2,text="DELETE",font=Font1, command=getvals1,padx=10,pady=10,bg='#2b2b2b',fg='#ffffff',activebackground='#686868',activeforeground='#ffffff').grid(row=6, column=3,padx=0,pady=10)
 
 Fontz = font.Font(family='Helvetica Neue', size=8 )
@@ -69,6 +80,5 @@ my_labelz2 = Label(root2, text='', font=Fontz)
 my_labelz2.grid(row=5,column=3)
 my_labelz3 = Label(root2, text='', font=Fontz)
 my_labelz3.grid(row=7,column=3)
-
 
 root2.mainloop()
